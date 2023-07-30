5bf3dab1ea7061c4b2fe9b791b9c0bbffcd582ce
Silva404 <brenosilva0252@gmail.com>
2023-07-30 19:16:45 -0300

    Base project

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,25 @@
 import { createRoot } from 'react-dom/client'
 import 'tailwindcss/tailwind.css'
-import App from 'components/App'
+import 'styles/global.css'
+import * as reactRouterDom from 'react-router-dom'
+import App, { OpenTab, OpenTabs, PlaceOrder } from 'App'
+import { Menu } from '@/features/menu/menu'
 
 const container = document.getElementById('root') as HTMLDivElement
 const root = createRoot(container)
-
-root.render(<App />)
+const router = reactRouterDom.createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { path: '/', element: <OpenTabs /> },
+      {
+        path: '/open-tab',
+        element: <OpenTab />
+      },
+      { path: '/place-order', element: <PlaceOrder /> },
+      { path: '/menu', element: <Menu /> }
+    ]
+  }
+])
+root.render(<reactRouterDom.RouterProvider router={router} />)
