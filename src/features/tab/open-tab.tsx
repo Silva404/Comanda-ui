3f93ee95f4b5343ded4394c0035fd8a8117fb9f5
Silva404 <brenosilva0252@gmail.com>
2023-08-13 22:24:26 -0300

    Remove typo

--- src/features/tab/open-tab.tsx
+++ src/features/tab/open-tab.tsx
@@ -133,7 +133,7 @@ export function OpenTab() {
           disabled={openTab.isLoading}
           className="flex w-full items-center justify-center gap-2 rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:w-auto"
         >
-          {!openTab.isLoading && <Spinner className="h-4" />} Abrir comanda
+          {openTab.isLoading && <Spinner className="h-4" />} Abrir comanda
         </button>
       </div>
     </form>
